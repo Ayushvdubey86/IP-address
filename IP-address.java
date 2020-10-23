eac6ba84e445319d475ad32b0512d8033b135866
Ayushvdubey86 <73361703+Ayushvdubey86@users.noreply.github.com>
2020-10-24 00:21:28 +0530

    Update IP-address.java

--- IP-address.java
+++ IP-address.java
@@ -8,7 +8,7 @@ class Ip
     try{  
           InetAddress ip=InetAddress.getByName("www.facebook.com");  
           System.out.println("Host Name: "+ip.getHostName());  
-          System.out.println("IP Address: "+ip.getHostAddress());  
+          System.out.println("IP Address of host name: "+ip.getHostAddress());  
        }catch(Exception e){System.out.println(e);}  
    }  
 }  
